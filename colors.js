895053be502d4b0f37c717dc9410426c6433ab4c
nimbus321 <34612874+nimbus321@users.noreply.github.com>
2023-03-05 19:59:12 -0500

    Create colors.js

---
+++ colors.js
@@ -0,0 +1,36 @@
+const C = { // ( Colors )
+  "&0": "\x1b[38;2;0;0;0m",
+  "&8": "\x1b[38;2;85;85;85m",
+  "&1": "\x1b[38;2;0;0;170m",
+  "&9": "\x1b[38;2;85;85;255m",
+  "&2": "\x1b[38;2;0;170;0m",
+  "&a": "\x1b[38;2;85;255;85m",
+  "&3": "\x1b[38;2;0;191;255m",
+  "&b": "\x1b[38;2;85;255;255m",
+  "&4": "\x1b[38;2;170;0;0m",
+  "&c": "\x1b[38;2;255;85;85m",
+  "&5": "\x1b[38;2;170;0;170m",
+  "&d": "\x1b[38;2;255;85;255m",
+  "&6": "\x1b[38;2;255;170;0m",
+  "&e": "\x1b[38;2;255;255;85m",
+  "&7": "\x1b[38;2;170;170;170m",
+  "&f": "\x1b[38;2;255;255;255m",
+  
+  "&r": "\x1b[0m", // reset r
+  reset: "\x1b[0m", // &r
+  "&n": "\x1b[4m", // underscore n
+  "&o": "\x1b[3m", // italic - o
+  "&m": "\x1b[9m", // strikethrough - m
+
+  "&l": "\x1b[1m", // bold l
+  bold: "\x1b[1m", // &l
+  background: "\x1b[7m", // background
+  "&bg": "\x1b[7m", // background
+  hidden: "\x1b[8m", // hidden
+  "&h": "\x1b[8m", // hidden - &h
+  italic: "\x1b[3m", // italic
+  "&i": "\x1b[3m", // italic
+  strikethrough: "\x1b[9m", // strikethrough - &m
+  "&str": "\x1b[9m", // strikethrough - &m
+  underscore: "\x1b[4m", // &n
+}
